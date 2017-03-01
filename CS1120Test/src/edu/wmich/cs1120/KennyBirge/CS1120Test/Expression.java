9e399c95968fc7c5fcc1fcdb1c9ee11ff450d217
Kenny Birge <Kenny Birge@10.80.147.154>
2017-03-01 11:15:49 -0500

    create new constructor for expression

--- CS1120Test/src/edu/wmich/cs1120/KennyBirge/CS1120Test/Expression.java
+++ CS1120Test/src/edu/wmich/cs1120/KennyBirge/CS1120Test/Expression.java
@@ -1,5 +1,15 @@
 package edu.wmich.cs1120.KennyBirge.CS1120Test;
 
 public class Expression {
-
+	private int op1, op2;
+	private char operand;
+	public Expression (int op1, int op2, char operand) {
+		this.op1 = op1;
+		this.op2 = op2;
+		this. operand = operand;
+	}
+	
+	public Expression() {
+		
+	}
 }
