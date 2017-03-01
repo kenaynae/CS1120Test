1c8ff9bdeac9001249d31aa07a4d6a71fb5b4237
Kenny Birge <Kenny Birge@10.80.147.154>
2017-03-01 11:11:44 -0500

    added exp

--- CS1120Test/src/edu/wmich/cs1120/KennyBirge/CS1120Test/TestMain.java
+++ CS1120Test/src/edu/wmich/cs1120/KennyBirge/CS1120Test/TestMain.java
@@ -3,7 +3,7 @@ package edu.wmich.cs1120.KennyBirge.CS1120Test;
 public class TestMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Expression exp = new Expression();
 
 	}
 
